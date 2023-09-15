f4bcf1a24034917c38e749d074a00bc7295cee07
Megan Hawkins <megan.hawkins28@gmail.com>
2023-09-15 12:32:43 -0400

    add style to layout

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -3,6 +3,10 @@ import type { FC } from "react";
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <></>;
+  return (
+    <>
+      <h1>Notes</h1>
+    </>
+  );
 };
 export default page;
